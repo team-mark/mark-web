de4d1485822fb886e87fa42a41067f22e877590e
KD <kdoobay@knights.ucf.edu>
2018-05-16 14:35:13 -0400

    Finished login stuff

--- src/public/pages/js/login.js
+++ src/public/pages/js/login.js
@@ -1,8 +1,30 @@
 const login = new Vue({
     el: '#login',
-    data: function() {
+    data: function () {
         return {
-        response: "null"
+            response: "null"
+        }
+    },
+    methods: {
+        submitForm: function(message, event) {
+            if(event) event.preventDefault();
+            this.$http.post("/accounts/login",
+                {
+                    handle: "handle",
+                    key: "key",
+
+                })
+                .then(function (response) {
+
+                    // get body data
+                    this.response = response.body;
+
+                },
+                    function (error) {
+                        // error callback
+                        console.log(error)
+                        this.response = error;
+                    });
         }
     }
 })
