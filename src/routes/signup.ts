ac305b9ebc734d192e6b2b67de1b41e7473b12e4
KD <kdoobay@knights.ucf.edu>
2018-06-23 15:57:39 -0400

    fixed follower nonsense

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -11,11 +11,7 @@ function index(req: express.Request, res: express.Response, next: express.NextFu
     res.render('signup', {
         title: 'Mark Mobile Verification',
         pageScript: 'signup',
-<<<<<<< HEAD
-        pageStyle: 'signup'
-=======
         pageStyle: 'signup',
         environment: JSON.stringify(localconfig.getLocalConfig())
->>>>>>> 005c52338acf8dd02e8e1722becdf9d9ab0a7249
     });
 }
