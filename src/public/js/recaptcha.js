f0d799efa4e0c50a7c4d61f1e87e111f96b8dd0f
Jake Ferrante <jake.ferrante@hotmail.com>
2018-08-01 00:52:58 -0400

    Fix recaptcha not loading properly

--- src/public/js/recaptcha.js
+++ src/public/js/recaptcha.js
@@ -1,32 +1,47 @@
 // Only have one recaptcha component on a page!
-var recaptchaLoaded = false;
+// var recaptchaLoaded = false;
 
-function reCaptchaOnLoad() {
-    recaptchaLoaded = true;
-}
+// function reCaptchaOnLoad() {
+//     recaptchaLoaded = true;
+// }
 
 Vue.component('recaptcha-component', {
-    props: ['active', 'modal_mode'], 
-    data: function() {
+    props: ['active', 'modal_mode'],
+    data: function () {
         return {
-            recaptchaWidget: 0, 
+            recaptchaWidget: 0,
             site_key: '6Le4OGUUAAAAALgJs_oHK5vEJGYyz9mQHn2NXpfM'
         }
     },
-    created: function() {
+    created: function () {
     },
-    mounted: function() {
+    mounted: function () {
+        // console.log('grecaptcha vue', grecaptcha)
         this.initReCaptcha();
     },
     methods: {
-        initReCaptcha: function() {
+        initReCaptcha: function () {
+            // console.log('grecaptcha init', grecaptcha)
+
             var self = this;
-            setTimeout(function() {
-                if(typeof grecaptcha === 'undefined') {
+            setTimeout(function () {
+                // if (typeof grecaptcha === 'undefined') {
+                //     self.initReCaptcha();
+                // }
+                // else {
+                //     console.log('this.site_key', this.site_key)
+                //     console.log('grecaptcha render', grecaptcha)
+                //     recaptchaWidget = grecaptcha.render('recaptcha', {
+                //         'sitekey': self.site_key,
+                //         'callback': self.captchaRes
+                //     });
+                // }
+                if (recaptchaLoaded === false) {
                     self.initReCaptcha();
                 }
                 else {
-                    console.log(this.site_key)
+                    console.log('this.site_key', this.site_key)
+                    console.log('grecaptcha render', grecaptcha)
                     recaptchaWidget = grecaptcha.render('recaptcha', {
                         'sitekey': self.site_key,
                         'callback': self.captchaRes
@@ -34,10 +49,10 @@ Vue.component('recaptcha-component', {
                 }
             }, 100);
         },
-        captchaRes: function(responseToken) {
+        captchaRes: function (responseToken) {
             this.$emit('captcha_result', responseToken);
         },
-        captchaExp: function() {
+        captchaExp: function () {
             // user needs to re verify
             this.$emit('captcha_expired');
         }
